9d2ca14ad5f1d4a6ccd940b474dbb810de2337a0
Astrid-Schneider <astridelizabethschneider@gmail.com>
2026-02-10 18:16:37 -0300

    tab

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -11,53 +11,64 @@ import { Title, Meta } from '@angular/platform-browser';
   styleUrl: './contact.css',
 })
 export class Contact {
-  constructor(private title: Title, private meta: Meta) {
-  this.title.setTitle('Contacto | H3 Logística de Campo');
 
-  this.meta.updateTag({
-    name: 'description',
-    content: 'Contactá a H3 Logística de Campo para asesoramiento en fumigaciones, agroquímicos y coadyuvantes agrícolas.'
-  });
-}
+  sending = false;
 
-sending = false;
+  model = {
+    name: '',
+    email: '',
+    message: '',
+  };
 
-model = {
-  name: '',
-  email: '',
-  message: '',
-};
+  constructor(
+    private title: Title,
+    private meta: Meta
+  ) {
+    this.title.setTitle('Contacto | H3 Logística de Campo');
 
-async submit() {
-  if (this.sending) return;
-  this.sending = true;
-
-const endpoint = 'https://formspree.io/f/mvzbgdjo';
-
-try {
-  const res = await fetch(endpoint, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name: this.model.name,
-      email: this.model.email,
-      message: this.model.message,
-    }),
-  });
-
-  if (res.ok) {
-    alert('Mensaje enviado correctamente');
-    this.model = { name: '', email: '', message: '' };
-  } else {
-    alert('Error al enviar el mensaje. Intentá nuevamente.');
+    this.meta.updateTag({
+      name: 'description',
+      content:
+        'Contactá a H3 Logística de Campo para asesoramiento en fumigaciones, agroquímicos y coadyuvantes agrícolas.',
+    });
   }
- } catch {
-  alert('Error de conexión. Revisá tu internet.');
- } finally {
-  this.sending = false;
- }
-}
+
+  async submit() {
+    if (this.sending) return;
+
+    this.sending = true;
+
+    const endpoint = 'https://formspree.io/f/mvzbgdjo';
+
+    try {
+      const res = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: this.model.name,
+          email: this.model.email,
+          message: this.model.message,
+        }),
+      });
+
+      if (res.ok) {
+        alert('Mensaje enviado correctamente');
+        this.model = {
+          name: '',
+          email: '',
+          message: '',
+        };
+      } else {
+        alert('Error al enviar el mensaje. Intentá nuevamente.');
+      }
+
+    } catch {
+      alert('Error de conexión. Revisá tu internet.');
+    } finally {
+      this.sending = false;
+    }
+  }
 }
